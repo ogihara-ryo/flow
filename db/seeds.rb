b0e00c82aa85f697614b130afd3f4b2b92f7dffd
ogihara-ryo <r-ogihara@froide-kk.co.jp>
2016-12-04 02:29:43 +0900

    Add seed for guest user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+User.connection.execute(%|SELECT setval ('users_id_seq', 1, false)|)
+User.create(name: 'guest', password: 'guest')
